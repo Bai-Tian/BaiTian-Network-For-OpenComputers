0b2c0d9d267417f5f941fdd72a62ef2119a8dbac
白天 <53045908+Bai-Tian@users.noreply.github.com>
2020-03-20 07:55:02 +0800

    地址配置文件

---
+++ SERVERSADDR.lua
@@ -0,0 +1,5 @@
+baitian_modem.serversaddr_version=0.1
+if not baitian_modem.serversaddr then baitian_modem.serversaddr = {} end
+baitian_modem.serversaddr.dns={}
+baitian_modem.serversaddr.relay={"24b3d0ee-fd85-411a-8715-da93a845636d"}
+baitian_modem.serversaddr.dns["tb"]="a709e51e-b923-49b0-96d4-7fd3073ba8ab"
